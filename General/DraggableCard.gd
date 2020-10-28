76497b87fa7f507d54e84626d8f2a58acaa24724
Allen Davis <allendaviskoonan@gmail.com>
2020-10-28 12:51:25 +0530

    Card Resource

--- General/DraggableCard.gd
+++ General/DraggableCard.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
-var is_dragged := false
+export (String, FILE) var card_packed_scene_path: String
 
+var is_dragged := false
+var card: Card = Card.new()
 
 func _ready() -> void:
 	connect("input_event", self, "_on_input_event")
-
+	card.packedScene = load(card_packed_scene_path)
 
 func _process(delta: float) -> void:
 	if is_dragged:
