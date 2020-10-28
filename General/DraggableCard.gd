cac2332ba9bb22c30667836670b1ff0fb2d0ec3a
Allen Davis <allendaviskoonan@gmail.com>
2020-10-28 14:18:14 +0530

    A useful script that sorts children

--- General/DraggableCard.gd
+++ General/DraggableCard.gd
@@ -1,5 +1,6 @@
-extends Area2D
 class_name DraggableCard
+extends Area2D
+
 
 export (String, FILE) var card_scene_path: String
 
@@ -12,29 +13,28 @@ onready var currentScene: Node = get_tree().current_scene
 
 func _ready() -> void:
 	card.scene = load(card_scene_path)
-	#signal connections
+	# signal connections
 	connect("input_event", self, "_on_input_event")
 	connect("area_entered", self, "_on_area_entered")
 	connect("area_exited", self, "_on_area_exited")	
-
+	
+	
 
 func _process(delta: float) -> void:
 	if is_dragged:
 		global_position = get_global_mouse_position()
-	else: #if the player stops dragging and the we are also in play area
+	else: # if the player stops dragging and the we are also in play area
 		if play_area_entered:	
-			_play()
+			play()
 
 
-#NON IN-BUILT FUNCTIONS
-func _play():
+func play():
 	var instance = card.scene.instance()
 	instance.global_position = global_position
 	currentScene.add_child(instance)
 	queue_free()
 
 
-#SIGNALS
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.pressed:
