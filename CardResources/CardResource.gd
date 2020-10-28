7b9ac66c62027a5fbfc37075df14bf3baf611765
Allen Davis <allendaviskoonan@gmail.com>
2020-10-28 13:06:37 +0530

    Basic state pattern

---
+++ CardResources/CardResource.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name Card
+
+var packed_scene_path: String
+var scene: PackedScene
--- General/DraggableCard.gd
+++ General/DraggableCard.gd
@@ -1,21 +1,66 @@
 extends Area2D
 
-export (String, FILE) var card_packed_scene_path: String
 
+enum States {
+	SUBSTITUTE,
+	PLAYING
+}
+
+
+export (String, FILE) var card_scene_path: String
+
+var state: int = States.SUBSTITUTE
+var play_area_entered := false
 var is_dragged := false
-var card: Card = Card.new()
+var card := Card.new()
+
 
 func _ready() -> void:
+	card.scene = load(card_scene_path)
+	#signal connections
 	connect("input_event", self, "_on_input_event")
-	card.packedScene = load(card_packed_scene_path)
+	connect("area_entered", self, "_on_area_entered")
+	connect("area_exited", self, "_on_area_exited")	
+
 
 func _process(delta: float) -> void:
-	if is_dragged:
-		global_position = get_global_mouse_position()
+	state_process()
+	
 
-
+func state_process() -> void:
+	match state:
+		States.SUBSTITUTE:
+			if is_dragged:
+				global_position = get_global_mouse_position()
+			
+			if play_area_entered:
+				if not is_dragged:
+					set_state(States.PLAYING)
+	
+		States.PLAYING:
+			pass	
+	
+	
+#SIGNALS
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event.is_action_pressed("drag"):
-		is_dragged = true
-	elif event.is_action_released("drag"):
-		is_dragged = false
+	if event is InputEventMouseButton:
+		if event.pressed:
+			is_dragged = true
+		else:
+			is_dragged = false
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("PlayArea"):
+		play_area_entered = true		
+
+
+func _on_area_exited(area: Area2D) -> void:
+	if area.is_in_group("PlayArea"):
+		play_area_entered = false		
+
+
+#SETTTERS AND GETTERS
+func set_state(new_state: int) -> void:
+	if state != new_state:
+		state = new_state
